e51d7072e54c61507d2b6164c89ea44a08911d2c
Rafael <eng.rsgomes@gmail.com>
2022-12-08 12:55:51 -0300

    add widget animation

--- components/Widget/Whatsapp.tsx
+++ components/Widget/Whatsapp.tsx
@@ -8,7 +8,7 @@ export function Widget() {
                 href="https://wa.me/5548984727432"
                 target="_blank"
                 rel="noreferrer"
-                className="bg-[#25d366] rounded-full p-5 text-white items-center flex group fixed bottom-5 right-5 lg:bottom-10 lg:right-10 drop-shadow-xl"
+                className="shaking bg-[#25d366] rounded-full p-3 lg:p-5 text-white items-center flex group fixed bottom-5 right-5 lg:bottom-10 lg:right-10 drop-shadow-xl"
             >
                 <FontAwesomeIcon icon={faWhatsapp} height={36} width={36} />
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs duration-700 whitespace-nowrap">
