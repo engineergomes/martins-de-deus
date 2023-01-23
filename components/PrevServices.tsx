a21f8f375d55ca4e58dac925537f0c84464469be
Rafael <eng.rsgomes@gmail.com>
2023-01-22 23:18:29 -0300

    remove coments

--- components/PrevServices.tsx
+++ components/PrevServices.tsx
@@ -45,55 +45,6 @@ function PrevServices() {
                         aposentadorias em geral, auxílio-acidente, LOAS e
                         auxílio-reclusão, e demais situações previdenciárias.
                     </p>
-
-                    {/* <div className="hidden lg:grid grid-cols-3 gap-10 place-items-center">
-                        {direitoCivil.map((item, index) => (
-                            <div
-                                key={index}
-                                className="grid grid-cols-1 grid-rows-[1fr,1fr,1fr] gap-0 rounded p-5 lg:p-10 w-full h-full place-items-center bg-[#fafafa] border border-transparent hover:border-[#d5a675] drop-shadow hover:drop-shadow-xl transition duration-300 ease-in-out"
-                            >
-                                <div className="h-16 w-16 lg:h-24 lg:w-24 fill-[#d5a675] relative">
-                                    {item.icon}
-                                </div>
-                                <h3 className="text-2xl font-bold text-center">
-                                    {item.name}
-                                </h3>
-                                <p className="text-center">{item.text}</p>
-                            </div>
-                        ))}
-                    </div> */}
-                    {/* <div className="flex flex-col gap-4 w-full lg:hidden">
-                        {direitoCivil.map((item, index) => (
-                            <Disclosure key={index}>
-                                {({ open }) => (
-                                    <>
-                                        <Disclosure.Button className="grid grid-cols-[1fr,10fr,1fr]  items-center rounded py-4 border-2 gap-4 px-4 hover:border-[#ffaa00] bg-[#3a563f] text-[#d5a675] fill-[#d5a675]">
-                                            <div className="h-8 w-8 relative">
-                                                {item.icon}
-                                            </div>
-                                            <p className="text-left">
-                                                {item.name}
-                                            </p>
-                                            <FontAwesomeIcon
-                                                icon={faCaretUp}
-                                                width={20}
-                                                height={20}
-                                                className={`${
-                                                    open
-                                                        ? "rotate-180 transform"
-                                                        : "rotate-90"
-                                                }
-                                            `}
-                                            />
-                                        </Disclosure.Button>
-                                        <Disclosure.Panel className="bg-[#d5a675] flex flex-col text-center p-4">
-                                            {item.text}
-                                        </Disclosure.Panel>
-                                    </>
-                                )}
-                            </Disclosure>
-                        ))}
-                    </div> */}
                 </div>
             </div>
         </>
