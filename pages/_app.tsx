4190d960cab9bea613b150d48d3429e68aaa094a
Rafael <eng.rsgomes@gmail.com>
2023-05-08 12:56:59 -0300

    change GTM tag, fix viewport meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,22 +8,37 @@ import Layout from "../components/Layout/Layout";
 import "aos/dist/aos.css";
 import "../styles/globals.css";
 import { Widget } from "../components/Widget/Whatsapp";
+import TagManager from "react-gtm-module";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-    useEffect(() => {
-        AOS.init({
-            once: true,
-            duration: 900,
-            easing: "ease-out-cubic",
-        });
-    });
+	useEffect(() => {
+		AOS.init({
+			once: true,
+			duration: 900,
+			easing: "ease-out-cubic",
+		});
+	});
+	useEffect(() => {
+		TagManager.initialize({ gtmId: "GTM-5PF3TMX" });
+	}, []);
 
-    return (
-        <Layout>
-            <>
-                <Component {...pageProps} />
-                <Widget />
-            </>
-        </Layout>
-    );
+	return (
+		<>
+			<Head>
+				{" "}
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				></meta>{" "}
+			</Head>
+
+			<Layout>
+				<>
+					<Component {...pageProps} />
+					<Widget />
+				</>
+			</Layout>
+		</>
+	);
 }
--- pages/_document.js
+++ pages/_document.js
@@ -2,78 +2,55 @@ import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default class Document extends NextDocument {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <Script
-                        src="https://www.googletagmanager.com/gtag/js?id=AW-350433948"
-                        strategy="afterInteractive"
-                    />
-                    <Script id="google-analytics" strategy="afterInteractive">
-                        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+	render() {
+		return (
+			<Html>
+				<Head>
+					<meta
+						name="google-site-verification"
+						content="a7JLH2XUmPvgX4X7Eojev9zKBVLX0KzsZfWHQ8IMkvE"
+					/>
+					<meta charSet="UTF-8" />
 
-          gtag('config', 'AW-350433948');
-        `}
-                    </Script>
-                    <meta
-                        name="google-site-verification"
-                        content="a7JLH2XUmPvgX4X7Eojev9zKBVLX0KzsZfWHQ8IMkvE"
-                    />
-                    <meta charset="UTF-8" />
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1.0"
-                    ></meta>
-                    <meta
-                        name="description"
-                        content="Martins de Deus Advogados Associados atuam nas áreas de família, cível, criminal, imobiliária e empresarial, entrega segurança jurídica em todas as demandas judiciais e extrajudiciais dos clientes."
-                    />
+					<meta
+						name="description"
+						content="Martins de Deus Advogados Associados atuam nas áreas de família, cível, criminal, imobiliária e empresarial, entrega segurança jurídica em todas as demandas judiciais e extrajudiciais dos clientes."
+					/>
 
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                        href="/apple-touch-icon.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="32x32"
-                        href="/favicon-32x32.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="16x16"
-                        href="/favicon-16x16.png"
-                    />
-                    <link rel="manifest" href="/site.webmanifest" />
-                    <link
-                        rel="mask-icon"
-                        href="/safari-pinned-tab.svg"
-                        color="#5bbad5"
-                    />
-                    <link rel="icon" href="/favicon.ico" />
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+					<link
+						rel="mask-icon"
+						href="/safari-pinned-tab.svg"
+						color="#5bbad5"
+					/>
+					<link rel="icon" href="/favicon.ico" />
 
-                    <meta name="msapplication-TileColor" content="#da532c" />
-                    <meta name="theme-color" content="#ffffff" />
-                </Head>
+					<meta name="msapplication-TileColor" content="#da532c" />
+					<meta name="theme-color" content="#ffffff" />
+				</Head>
 
-                <body>
-                    {/* <noscript
-                        dangerouslySetInnerHTML={{
-                            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5TJPJLF"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-                        }}
-                    ></noscript> */}
-
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
