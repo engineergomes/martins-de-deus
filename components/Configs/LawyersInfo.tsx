d39332fdf2c25d1740fe90792b6eb7c8ac1d16f1
Rafael <eng.rsgomes@gmail.com>
2023-10-23 23:45:02 -0300

    rm lawyers

--- components/Configs/LawyersInfo.tsx
+++ components/Configs/LawyersInfo.tsx
@@ -12,14 +12,14 @@ export const lawyers = [
 		),
 		about: "Manuela Martins é advogada, graduada pela Universidade do Vale do Itajaí, pós-graduada em Direito Civil pela Anhanguera, proprietária do Escritório Martins de Deus Advogados Associados, atuante nas áreas de Direito Civil, Direito de Família, Direito do Consumidor e Direito Imobiliário. Na prática da advocacia preza sempre pela ética, buscando prestar um atendimento humano e personalizado na busca do direito de seus clientes.",
 	},
-	{
-		name: "SIBELLE LOHN",
-		imgSrc: "/img6.png",
-		oab: "OAB/SC 56.508",
-		graduation: "Graduada pela Universidade do Vale do Itajaí",
-		pos: "Atuação familiarista com ênfase na Defesa do Direito das Mulheres",
-		about: "Sibelle Lohn é advogada, graduada pela Universidade do Vale do Itajaí, associada do Escritório Martins de Deus Advogados Associados, atuante nas áreas de Direito Civil e Direito de Família com foco na atuação em defesa do direito das mulheres. Na prática da advocacia, preza por um atendimento humanizado, acolhedor e responsável, aplicando o direito de forma crítica, considerando as subjetividades humanas, sempre buscando entender a fundo a necessidade dos clientes de forma personalizada.",
-	},
+	// {
+	// 	name: "SIBELLE LOHN",
+	// 	imgSrc: "/img6.png",
+	// 	oab: "OAB/SC 56.508",
+	// 	graduation: "Graduada pela Universidade do Vale do Itajaí",
+	// 	pos: "Atuação familiarista com ênfase na Defesa do Direito das Mulheres",
+	// 	about: "Sibelle Lohn é advogada, graduada pela Universidade do Vale do Itajaí, associada do Escritório Martins de Deus Advogados Associados, atuante nas áreas de Direito Civil e Direito de Família com foco na atuação em defesa do direito das mulheres. Na prática da advocacia, preza por um atendimento humanizado, acolhedor e responsável, aplicando o direito de forma crítica, considerando as subjetividades humanas, sempre buscando entender a fundo a necessidade dos clientes de forma personalizada.",
+	// },
 	{
 		name: "VICTOR FRANCISCO AGUIAR DE MELO",
 		imgSrc: "/img5.png",
@@ -53,12 +53,12 @@ export const lawyers = [
 			"Graduanda da Universidade do Sul de Santa Catarina - UNISUL",
 		about: "A Maria é estudante de direito e, como estagiária, auxilia os demais membros do escritório na produção de peças jurídicas, elaboração de documentos e diligências. Com rápido aprendizado e grande interesse na prática da advocacia, possui um futuro brilhante como operadora do direito.",
 	},
-	{
-		name: "DANIEL DA CUNHA JUNIOR",
-		imgSrc: "/img14.png",
-		oab: "Estagiário de Direito",
-		graduation:
-			"Graduando da Universidade do Sul de Santa Catarina - UNISUL",
-		about: "Daniel é estudante de direito e, como estagiário, auxilia os demais membros do escritório na produção de peças jurídicas, elaboração de documentos e diligências. Com raciocínio rápido e objetivo, desenvolve as tarefas que lhe são confiadas com agilidade e assertividade e tem uma carreira de muito sucesso pela frente.",
-	},
+	// {
+	// 	name: "DANIEL DA CUNHA JUNIOR",
+	// 	imgSrc: "/img14.png",
+	// 	oab: "Estagiário de Direito",
+	// 	graduation:
+	// 		"Graduando da Universidade do Sul de Santa Catarina - UNISUL",
+	// 	about: "Daniel é estudante de direito e, como estagiário, auxilia os demais membros do escritório na produção de peças jurídicas, elaboração de documentos e diligências. Com raciocínio rápido e objetivo, desenvolve as tarefas que lhe são confiadas com agilidade e assertividade e tem uma carreira de muito sucesso pela frente.",
+	// },
 ];
