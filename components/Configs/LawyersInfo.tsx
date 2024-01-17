efcf9b68f8ea26b48961f9d1e1135f3bf7737a29
Rafael Lima <rafaelvasconcelos@protonmail.com>
2024-01-17 15:45:08 -0300

    victor aguiar info updated

--- components/Configs/LawyersInfo.tsx
+++ components/Configs/LawyersInfo.tsx
@@ -1,73 +1,78 @@
 export const lawyers = [
-	{
-		name: "Manuela Martins de Deus",
-		imgSrc: "/img3.png",
-		oab: "OAB/SC 52.343",
-		graduation: "Graduada pela Universidade do Vale do Itajaí",
-		pos: (
-			<div className="flex flex-col gap-1">
-				<p>Pós Graduada em Direito Civil pela LFG</p>
-				<p>Pós-graduanda em direito imobiliário</p>
-			</div>
-		),
-		about: "Manuela Martins é advogada, graduada pela Universidade do Vale do Itajaí, pós-graduada em Direito Civil pela Anhanguera, proprietária do Escritório Deus e Aguiar Advocacia, atuante nas áreas de Direito Civil, Direito de Família, Direito do Consumidor e Direito Imobiliário. Na prática da advocacia preza sempre pela ética, buscando prestar um atendimento humano e personalizado na busca do direito de seus clientes.",
-	},
-	// {
-	// 	name: "SIBELLE LOHN",
-	// 	imgSrc: "/img6.png",
-	// 	oab: "OAB/SC 56.508",
-	// 	graduation: "Graduada pela Universidade do Vale do Itajaí",
-	// 	pos: "Atuação familiarista com ênfase na Defesa do Direito das Mulheres",
-	// 	about: "Sibelle Lohn é advogada, graduada pela Universidade do Vale do Itajaí, associada do Escritório Deus e Aguiar Advocacia, atuante nas áreas de Direito Civil e Direito de Família com foco na atuação em defesa do direito das mulheres. Na prática da advocacia, preza por um atendimento humanizado, acolhedor e responsável, aplicando o direito de forma crítica, considerando as subjetividades humanas, sempre buscando entender a fundo a necessidade dos clientes de forma personalizada.",
-	// },
-	{
-		name: "VICTOR FRANCISCO AGUIAR DE MELO",
-		imgSrc: "/img5.png",
-		oab: "OAB/SC 62.016",
-		graduation: "Graduado pela Universidade do Vale do Itajaí",
-		pos: "Pós-graduando em direito empresarial",
-		about: "Victor Aguiar de Melo é advogado, graduado em Direito pela Universidade do Vale do Itajaí, associado do Escritório Deus e Aguiar Advocacia, atuante na área do Direito Civil, com especial enfoque nas relações de consumo, no assessoramento empresarial e nas análises contratuais. Na prática da advocacia, preza por um atendimento digno e diferenciado aos clientes com uma atuação profissional com elevado senso crítico, ética, e uma busca constante pela melhor alternativa e pela Garantia dos Direitos dos clientes.",
-	},
-	{
-		name: "LEONORA SOUZA",
-		imgSrc: "/img7.png",
-		oab: "OAB/SC 66.629",
-		graduation: "Graduada pela Universidade do Vale do Itajaí ",
-		pos: "Pós-graduanda em Criminologia e Direito Penal pela PUC do Rio Grande do Sul",
-		about: "Leonora Souza é advogada, graduada pela Universidade do Vale do Itajaí e especializanda em Criminologia e Direito Penal pela PUC do Rio Grande do Sul, associada do Escritório Deus e Aguiar Advocacia, atuante na área de Direito Penal e Processual Penal com foco especial em processos criminais comuns e crimes da lei especial de tóxicos. Na prática da advocacia, preza por um atendimento digno e respeitoso com enfoque na garantia dos direitos humanos do acusado e das vítimas.",
-	},
-	{
-		name: "Marcella Schmitz",
-		imgSrc: "/img8.png",
-		oab: "OAB/SC 52.462",
-		graduation: "Graduada pela Universidade do Vale do Itajaí ",
-		pos: "Pós-graduanda em Direito do Trabalho e Previdenciária na Associação de Magistrados do Trabalho",
-		about: "Marcella Schmitz é advogada, graduada pela Universidade do Vale do Itajaí, com especialização em Direito Previdenciário pelo Instituto de Estudos Previdenciários - IEPREV, e pós-graduanda em Direito do Trabalho e Previdenciária na Associação de Magistrados do Trabalho da 12ª Região - AMATRA12. Atuante há quase 5 anos nas respectivas áreas, prima pelo atendimento humanizado e atuação proativa, para melhor defender e atender o interesse de seus clientes.",
-	},
+  {
+    name: "Manuela Martins de Deus",
+    imgSrc: "/img3.png",
+    oab: "OAB/SC 52.343",
+    graduation: "Graduada pela Universidade do Vale do Itajaí",
+    pos: (
+      <div className="flex flex-col gap-1">
+        <p>Pós Graduada em Direito Civil pela LFG</p>
+        <p>Pós-graduanda em direito imobiliário</p>
+      </div>
+    ),
+    about:
+      "Manuela Martins é advogada, graduada pela Universidade do Vale do Itajaí, pós-graduada em Direito Civil pela Anhanguera, proprietária do Escritório Deus e Aguiar Advocacia, atuante nas áreas de Direito Civil, Direito de Família, Direito do Consumidor e Direito Imobiliário. Na prática da advocacia preza sempre pela ética, buscando prestar um atendimento humano e personalizado na busca do direito de seus clientes.",
+  },
+  // {
+  // 	name: "SIBELLE LOHN",
+  // 	imgSrc: "/img6.png",
+  // 	oab: "OAB/SC 56.508",
+  // 	graduation: "Graduada pela Universidade do Vale do Itajaí",
+  // 	pos: "Atuação familiarista com ênfase na Defesa do Direito das Mulheres",
+  // 	about: "Sibelle Lohn é advogada, graduada pela Universidade do Vale do Itajaí, associada do Escritório Deus e Aguiar Advocacia, atuante nas áreas de Direito Civil e Direito de Família com foco na atuação em defesa do direito das mulheres. Na prática da advocacia, preza por um atendimento humanizado, acolhedor e responsável, aplicando o direito de forma crítica, considerando as subjetividades humanas, sempre buscando entender a fundo a necessidade dos clientes de forma personalizada.",
+  // },
+  {
+    name: "VICTOR FRANCISCO AGUIAR DE MELO",
+    imgSrc: "/img5.png",
+    oab: "OAB/SC 62.016",
+    graduation: "Graduado pela Universidade do Vale do Itajaí",
+    pos: "Pós-graduando em direito empresarial",
+    about:
+      "Victor Aguiar de Melo é advogado, graduado em Direito pela Universidade do Vale do Itajaí, associado do Escritório Deus e Aguiar Advocacia, atuante na área do Direito Civil, com especial enfoque nas relações de consumo, no assessoramento empresarial e nas análises contratuais. Na prática da advocacia, preza por um atendimento digno e diferenciado aos clientes com uma atuação profissional com elevado senso crítico, ética, e uma busca constante pela melhor alternativa e pela Garantia dos Direitos dos clientes.",
+  },
+  {
+    name: "LEONORA SOUZA",
+    imgSrc: "/img7.png",
+    oab: "OAB/SC 66.629",
+    graduation: "Graduada pela Universidade do Vale do Itajaí ",
+    pos: "Pós-graduanda em Criminologia e Direito Penal pela PUC do Rio Grande do Sul",
+    about:
+      "Leonora Souza é advogada, graduada pela Universidade do Vale do Itajaí e especializanda em Criminologia e Direito Penal pela PUC do Rio Grande do Sul, associada do Escritório Deus e Aguiar Advocacia, atuante na área de Direito Penal e Processual Penal com foco especial em processos criminais comuns e crimes da lei especial de tóxicos. Na prática da advocacia, preza por um atendimento digno e respeitoso com enfoque na garantia dos direitos humanos do acusado e das vítimas.",
+  },
+  {
+    name: "Marcella Schmitz",
+    imgSrc: "/img8.png",
+    oab: "OAB/SC 52.462",
+    graduation: "Graduada pela Universidade do Vale do Itajaí ",
+    pos: "Pós-graduanda em Direito do Trabalho e Previdenciária na Associação de Magistrados do Trabalho",
+    about:
+      "Marcella Schmitz é advogada, graduada pela Universidade do Vale do Itajaí, com especialização em Direito Previdenciário pelo Instituto de Estudos Previdenciários - IEPREV, e pós-graduanda em Direito do Trabalho e Previdenciária na Associação de Magistrados do Trabalho da 12ª Região - AMATRA12. Atuante há quase 5 anos nas respectivas áreas, prima pelo atendimento humanizado e atuação proativa, para melhor defender e atender o interesse de seus clientes.",
+  },
 
-	{
-		name: "MARIA EDUARDA NEUHAUS",
-		imgSrc: "/img4.png",
-		oab: "Estagiária de Direito",
-		graduation:
-			"Graduanda da Universidade do Sul de Santa Catarina - UNISUL",
-		about: "A Maria é estudante de direito e, como estagiária, auxilia os demais membros do escritório na produção de peças jurídicas, elaboração de documentos e diligências. Com rápido aprendizado e grande interesse na prática da advocacia, possui um futuro brilhante como operadora do direito.",
-	},
+  {
+    name: "MARIA EDUARDA NEUHAUS",
+    imgSrc: "/img4.png",
+    oab: "Estagiária de Direito",
+    graduation: "Graduanda da Universidade do Sul de Santa Catarina - UNISUL",
+    about:
+      "A Maria é estudante de direito e, como estagiária, auxilia os demais membros do escritório na produção de peças jurídicas, elaboração de documentos e diligências. Com rápido aprendizado e grande interesse na prática da advocacia, possui um futuro brilhante como operadora do direito.",
+  },
 
-	{
-		name: "GABRIELA SCHMITZ GOMES",
-		imgSrc: "/img17.png",
-		oab: "Assessora Jurídica",
-		graduation: "Bacharel em Direito pela Universidade do Vale do Itajaí",
-		about: "Assessora os demais membros da equipe na produção de peças jurídicas, elaboração de documentos e diligências. Na prática da advocacia contribui para fortalecer o entendimento dos direitos e valores da sociedade. Buscando prezar pela equidade e verdade.",
-	},
+  {
+    name: "GABRIELA SCHMITZ GOMES",
+    imgSrc: "/img17.png",
+    oab: "Assessora Jurídica",
+    graduation: "Bacharel em Direito pela Universidade do Vale do Itajaí",
+    about:
+      "Assessora os demais membros da equipe na produção de peças jurídicas, elaboração de documentos e diligências. Na prática da advocacia contribui para fortalecer o entendimento dos direitos e valores da sociedade. Buscando prezar pela equidade e verdade.",
+  },
 
-	// {
-	// 	name: "DANIEL DA CUNHA JUNIOR",
-	// 	imgSrc: "/img14.png",
-	// 	oab: "Estagiário de Direito",
-	// 	graduation:
-	// 		"Graduando da Universidade do Sul de Santa Catarina - UNISUL",
-	// 	about: "Daniel é estudante de direito e, como estagiário, auxilia os demais membros do escritório na produção de peças jurídicas, elaboração de documentos e diligências. Com raciocínio rápido e objetivo, desenvolve as tarefas que lhe são confiadas com agilidade e assertividade e tem uma carreira de muito sucesso pela frente.",
-	// },
+  // {
+  // 	name: "DANIEL DA CUNHA JUNIOR",
+  // 	imgSrc: "/img14.png",
+  // 	oab: "Estagiário de Direito",
+  // 	graduation:
+  // 		"Graduando da Universidade do Sul de Santa Catarina - UNISUL",
+  // 	about: "Daniel é estudante de direito e, como estagiário, auxilia os demais membros do escritório na produção de peças jurídicas, elaboração de documentos e diligências. Com raciocínio rápido e objetivo, desenvolve as tarefas que lhe são confiadas com agilidade e assertividade e tem uma carreira de muito sucesso pela frente.",
+  // },
 ];
